7e02a943d8a92fa888293c5b565f883a0ce8c91f
IkramAnsari01 <ikramansari21112001@gmail.com>
2026-04-23 14:19:31 +0530

    Update trivy_scan.groovy

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,3 +1,21 @@
-def call(){
-  sh "trivy fs ."
+def call() {
+    sh """
+        echo "Pulling Trivy image..."
+        docker pull aquasec/trivy:latest
+
+        echo "Running Trivy scan..."
+        docker run --rm \
+            -v /var/run/docker.sock:/var/run/docker.sock \
+            -v \$(pwd):/workspace \
+            -w /workspace \
+            aquasec/trivy:latest fs . \
+            --severity HIGH,CRITICAL \
+            --ignore-unfixed \
+            --format table \
+            --output trivy-report.txt \
+            --exit-code 0
+
+        echo "Removing Trivy image..."
+        docker rmi aquasec/trivy:latest || true
+    """
 }
